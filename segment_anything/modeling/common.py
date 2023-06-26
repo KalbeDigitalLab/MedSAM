b42f8dc2ea0c8c8c9193d97d09523d23e9967ea0
haritsah <m.haritsah@mail.ugm.ac.id>
2023-06-26 16:31:12 +0700

    Add lora model on image encoder

--- segment_anything/modeling/common.py
+++ segment_anything/modeling/common.py
@@ -10,6 +10,42 @@ import torch.nn as nn
 from typing import Type
 
 
+class LoRALayer_qkv_timm(nn.Module):
+    """In timm it is implemented as
+    self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+
+    B, N, C = x.shape
+    qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, self.head_dim).permute(2, 0, 3, 1, 4)
+    q, k, v = qkv.unbind(0)
+
+    """
+
+    def __init__(
+        self,
+        qkv: nn.Module,
+        linear_a_q: nn.Module,
+        linear_b_q: nn.Module,
+        linear_a_v: nn.Module,
+        linear_b_v: nn.Module,
+    ):
+        super().__init__()
+        self.qkv = qkv
+        self.linear_a_q = linear_a_q
+        self.linear_b_q = linear_b_q
+        self.linear_a_v = linear_a_v
+        self.linear_b_v = linear_b_v
+        self.dim = qkv.in_features
+        self.w_identity = torch.eye(qkv.in_features)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        qkv = self.qkv(x)  # B,N,3*org_C
+        new_q = self.linear_b_q(self.linear_a_q(x))
+        new_v = self.linear_b_v(self.linear_a_v(x))
+        qkv[:, :, : self.dim] += new_q
+        qkv[:, :, -self.dim :] += new_v
+        return qkv
+
+
 class MLPBlock(nn.Module):
     def __init__(
         self,
